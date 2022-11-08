4ff4ca7a01a8ac5359b3697b005f777763870432
rSathish69 <rsathishrajasekar@gmail.com>
2022-11-08 14:34:32 +0530

    floating action button and Bottom app bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,82 +3,88 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
 
-  List<String> products = ["Bed", "Chair", "Sofa"];
-  List<String> productDetails = [
-    "king size bed",
-    "wooden chair",
-    "king size sofa"
-  ];
-  List<int> price = [300, 200, 150];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow.shade500,
       appBar: AppBar(
-        title: Text("Navigation Drawer"),
+          title: Text("Floating Action Button"),
+          centerTitle: true),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.cyanAccent,
+        elevation: 20.0,
+        // shape: BeveledRectangleBorder(
+        //   borderRadius: BorderRadius.circular(30.0),
+        //   side: BorderSide(color: Colors.amber,width: 2.0,style: BorderStyle.solid)
+        // ),
+        // mini: true,
       ),
-      drawer: Drawer(
-        child: ListView(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        shape: CircularNotchedRectangle(),
+        color: Colors.black87,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            UserAccountsDrawerHeader(
-              accountEmail: Text("rsathishrajasekar@gmail.com"),
-              accountName: Text("SATHISH"),
-              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage("PROFILE.jpeg")),
-              otherAccountsPictures: [
-                CircleAvatar(foregroundImage: AssetImage("google.jpg")),
-                CircleAvatar(foregroundImage: AssetImage("linkdin.webp")),
-              ],
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.shopping_cart),
-              title: Text("shop"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text("favorites"),
-              onTap: () {},
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.home,
+                  color: Colors.white),
+                  Text("home",
+                  style: TextStyle(color: Colors.white),)
+
+                ],
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text("Labels"),
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.shopping_cart,
+                  color: Colors.white),
+                  Text("cart",
+                  style: TextStyle(color: Colors.white),)
+
+                ],
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.label),
-              title: Text("red"),
-              onTap: () {},
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.favorite,
+                  color: Colors.white),
+                  Text("fav",
+                  style: TextStyle(color: Colors.white),)
+
+                ],
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.label),
-              title: Text("green"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.label),
-              title: Text("yellow"),
-              onTap: () {},
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.settings,
+                  color: Colors.white),
+                  Text("setting",
+                  style: TextStyle(color: Colors.white),)
+
+                ],
+              ),
             ),
           ],
-        ),
-      ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                child: Text(products[index][0]),
-              ),
-              title: Text(products[index]),
-              subtitle: Text(productDetails[index]),
-              trailing: Text(price[index].toString()),
-            );
-          },
         ),
       ),
     );
